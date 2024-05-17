0606502465f3ef639ca32cdfd1d76b5215808ff2
vitordahoraa <93230828+vitordahoraa@users.noreply.github.com>
2024-05-17 01:10:47 -0300

    Finalized Warnsdorf's rule implementation in GOLANG

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 var validKnightsMove = [][]int8{
@@ -13,6 +14,21 @@ var validKnightsMove = [][]int8{
 	{-1, -2},
 	{-2, 1},
 	{-2, -1},
+}
+
+// Validar se o tabueleiro esta todo feito
+func validaTabuleiro(slice2Dint_tabuleiro [][]int8) bool {
+	haPosicoesNaoVisitadas := false
+
+	for i := range slice2Dint_tabuleiro {
+		for j := range slice2Dint_tabuleiro[i] {
+			if !haPosicoesNaoVisitadas {
+				haPosicoesNaoVisitadas = slice2Dint_tabuleiro[i][j] == -1
+			}
+		}
+	}
+
+	return !haPosicoesNaoVisitadas
 }
 
 func retornaMovimentosValidos(slice2Dint_tabuleiro [][]int8, int_posicaoLinhaAtual uint8, int_posicaoColunaAtual uint8) [][]uint8 {
@@ -34,7 +50,9 @@ func retornaMovimentosValidos(slice2Dint_tabuleiro [][]int8, int_posicaoLinhaAtu
 		positionValidar[0] = uint8(linhaIteration)
 		positionValidar[1] = uint8(colunaIteration)
 
-		slice_movimentosValidos = append(slice_movimentosValidos, positionValidar)
+		if slice2Dint_tabuleiro[positionValidar[0]][positionValidar[1]] == -1 {
+			slice_movimentosValidos = append(slice_movimentosValidos, positionValidar)
+		}
 	}
 	return slice_movimentosValidos
 }
@@ -76,16 +94,32 @@ func criarTabuleiro(int_tamanhoTabuleiro uint8) [][]int8 {
 	return slice2Dint_tabuleiro
 }
 
-func visitarTodoTabuleiro(slice2Dint_tabuleiro [][]int8, int_linhaInicial uint8, int_colunaInicial uint8) [][]int8 {
+// Funcao utilizada pra retornar o numero de posicoes validas aparece na slice de movimentosValidos
+func numeroDeInstanciasDeMovimentosValidos(slice_movimentosValidos []int8) map[int8]int8 {
+	dict := make(map[int8]int8)
+	for _, num := range slice_movimentosValidos {
+		dict[num] = dict[num] + 1
+	}
+	return dict
+}
 
-	//Visitar posicao inicial
-	slice2Dint_tabuleiro[int_linhaInicial][int_colunaInicial] = 1
-
-	uint8_posicaoLinhaAtual := int_linhaInicial
-	uint8_posicaoColunaAtual := int_colunaInicial
+func visitarPosicaoTabuleiro(slice2Dint_tabuleiro [][]int8, uint8_posicaoLinhaAtual uint8, uint8_posicaoColunaAtual uint8, int8_numeroVisita int8) bool {
 
 	//Verifica movimentos validos dessa posicao
 	slice_movimentosValidos := retornaMovimentosValidos(slice2Dint_tabuleiro, uint8_posicaoLinhaAtual, uint8_posicaoColunaAtual)
+
+	//Visitar posicao
+	slice2Dint_tabuleiro[uint8_posicaoLinhaAtual][uint8_posicaoColunaAtual] = int8_numeroVisita
+
+	printTabuleiroAndWaitUserInput(slice2Dint_tabuleiro)
+
+	//Caso essa posicao nao seja valida, retornar true caso o tabuleiro esteja completo, e false caso contrario
+	if len(slice_movimentosValidos) == 0 {
+		return validaTabuleiro(slice2Dint_tabuleiro)
+	}
+
+	//Incrementa a visita
+	int8_numeroVisita++
 
 	slice_proxMovimentos := make([]int8, len(slice_movimentosValidos))
 
@@ -95,13 +129,41 @@ func visitarTodoTabuleiro(slice2Dint_tabuleiro [][]int8, int_linhaInicial uint8,
 		//slice2Dint_tabuleiro[slice_movimentosValidos[posicao][0]][slice_movimentosValidos[posicao][1]] = int8(len(retornaMovimentosValidos(slice2Dint_tabuleiro, slice_movimentosValidos[posicao][0], slice_movimentosValidos[posicao][1])))
 	}
 
-	for posicao := range slice_movimentosValidos {
+	//slice_numVezes := numeroDeInstanciasDeMovimentosValidos(slice_proxMovimentos)
+
+	// Sorteia do menor ao maior
+	sort.SliceStable(slice_proxMovimentos, func(i, j int) bool { return slice_proxMovimentos[i] < slice_proxMovimentos[j] })
+
+	//Criar variavel para iterar sobre todas as casas que tem a mesma casas validas
+	//contadorNumVezes := 0
+	//Para cada movimento proximo, do menor para o maior
+	for proxMov := range slice_proxMovimentos {
+		//Inicia contador de vezes dessa iteracao
+		//contadorNumVezesIteration := 0
+		for posicao := range slice_movimentosValidos {
+			casasValidas := int8(len((retornaMovimentosValidos(slice2Dint_tabuleiro, slice_movimentosValidos[posicao][0], slice_movimentosValidos[posicao][1]))))
+			//Caso o numero de casas validas seja o menor iterado
+			if casasValidas == slice_proxMovimentos[proxMov] {
+				// Se retornou false
+				fmt.Println(int8_numeroVisita)
+				if !visitarPosicaoTabuleiro(slice2Dint_tabuleiro, slice_movimentosValidos[posicao][0], slice_movimentosValidos[posicao][1], int8_numeroVisita) {
+					continue
+				}
+
+				break
+			}
+		}
 		// Colocar a posicao valida de todas as posicoes
-		slice2Dint_tabuleiro[slice_movimentosValidos[posicao][0]][slice_movimentosValidos[posicao][1]] = int8(slice_proxMovimentos[posicao])
+		//slice2Dint_tabuleiro[slice_movimentosValidos[posicao][0]][slice_movimentosValidos[posicao][1]] = int8(slice_proxMovimentos[posicao])
+		//
 	}
 
-	return slice2Dint_tabuleiro
+	return true
+}
 
+func visitarTodoTabuleiro(slice2Dint_tabuleiro [][]int8, uint8_posicaoInicialLinha uint8, uint8_posicaoInicialColuna uint8) {
+	//Visitar primeira posicao
+	visitarPosicaoTabuleiro(slice2Dint_tabuleiro, uint8_posicaoInicialLinha, uint8_posicaoInicialColuna, 1)
 }
 
 // Printar o tabuleiro
@@ -111,14 +173,14 @@ func printTabuleiroAndWaitUserInput(slice2Dint_tabuleiro [][]int8) {
 
 	for i := range slice2Dint_tabuleiro {
 		for j := range slice2Dint_tabuleiro[i] {
-			fmt.Printf("%d ", slice2Dint_tabuleiro[i][j])
+			fmt.Printf("%d  ", slice2Dint_tabuleiro[i][j])
 		}
 		fmt.Print("\n")
 	}
 
 	//Wait user input
-	fmt.Print("Aperte uma tecla para continuar!\n")
-	fmt.Scanln()
+	//fmt.Print("Aperte uma tecla para continuar!\n")
+	//fmt.Scanln()
 }
 
 func main() {
@@ -133,8 +195,6 @@ func main() {
 	// -1 = nao visitado, qualquer outro valor eh a ordem de visitacao
 	slice2Dint_tabuleiro := criarTabuleiro(int_tamanhoTabuleiro)
 
-	slice2Dint_tabuleiro = visitarTodoTabuleiro(slice2Dint_tabuleiro, int_linhaInicial, int_colunaInicial)
-
-	printTabuleiroAndWaitUserInput(slice2Dint_tabuleiro)
+	visitarTodoTabuleiro(slice2Dint_tabuleiro, int_linhaInicial, int_colunaInicial)
 
 }
